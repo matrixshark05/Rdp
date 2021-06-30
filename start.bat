f1b087efffad265eddee8ded03dc4533a904a290
matrixshark05 <82001803+matrixshark05@users.noreply.github.com>
2021-06-30 10:49:17 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,9 @@
-@echo off
+  
+@ echo  off
 
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 
-net config server /srvcomment:"Windows Server 2019 By Exynos Admin" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Matrix Admin" > out.txt 2>&1
 
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 
@@ -16,26 +17,26 @@ net user administrator /active:yes >nul
 
 net user installer /delete
 
-diskperf -Y >nul
+diskperf -Y > null
 
 sc config Audiosrv start= auto >nul
 
-sc start audiosrv >nul
+sc  start audiosrv > null
 
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 
 ICACLS C:\Windows\installer /grant administrator:F >nul
 
-echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
+echo Installed Successfully !, If RDP is Dead Please Rebuild Again Ok!
 
 echo IP:
 
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+tasklist  |  find /i " ngrok.exe "  > Nul  && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url ||  echo  " Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 
 echo Username: administrator
 
 echo Password: Matrix@rdp123
 
-echo Silahkan Login Ke RDP Anda Sekarang
+echo Please Login to Your RDP Now
 
-ping -n 10 127.0.0.1 >nul
+ping -n 10 127.0.0.1 > null
