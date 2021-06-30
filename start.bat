84b56900b5abfa26c9ca965829fa6b61c6d268a3
matrixshark05 <82001803+matrixshark05@users.noreply.github.com>
2021-06-30 22:33:29 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -31,7 +31,7 @@ echo Installed Successfully !, If RDP is Dead Please Rebuild Again Ok!
 
 echo IP:
 
-tasklist  |  find /i " ngrok.exe "  > Nul  && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url ||  echo  " Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 
 echo Username: administrator
 
